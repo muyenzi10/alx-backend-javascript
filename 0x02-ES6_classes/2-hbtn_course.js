d6cddd0bac82fbc37ac5c78825fb6f509773a613
muyenzi10 <muyenzija10@gmail.com>
2024-06-10 10:23:25 -0700

    updated

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,64 @@
+export default class HolbertonCourse {
+  /**
+   * Createses
+   *
+   * @param 
+   * @pa
+   * @param {String[]} students - The names of students in the course.
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /**
+   * Gets the name of this course.
+   */
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /**
+   * Gets the length of this course (in months).
+   */
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * Sets the length of this course (in months).
+   */
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  /**
+   * Geting 
+   */
+  get students() {
+    return this._students;
+  }
+
+  /**
+   * Seting array
+   */
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
